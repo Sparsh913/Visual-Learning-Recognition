5c591e267398453c6c928fe0cabfe69f58012058
Sparsh Garg <sparshg@andrew.cmu.edu>
2024-03-16 14:17:50 -0400

    gan and vae done

---
+++ hw2/generative-modeling/gan/q1_3.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+from utils import get_args
+
+import torch
+
+from networks import Discriminator, Generator
+import torch.nn.functional as F
+from train import train_model
+
+
+def compute_discriminator_loss(
+    discrim_real, discrim_fake, discrim_interp, interp, lamb
+):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for discriminator.
+    # Do not use discrim_interp, interp, lamb. They are placeholders
+    # for Q1.5.
+    ##################################################################
+    loss = None
+    # implement the loss from https://arxiv.org/pdf/1406.2661.pdf
+    loss = -torch.mean(torch.log(abs(discrim_real)) + torch.log(abs(1 - discrim_fake)))
+    # print("dicrim_real", discrim_real)
+    # print("discrim_fake", discrim_fake)
+    # print("loss discriminator", loss)
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+def compute_generator_loss(discrim_fake):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for the generator.
+    ##################################################################
+    # loss = None
+    # implement the loss from https://arxiv.org/pdf/1406.2661.pdf
+    loss = -torch.mean(torch.log(abs(discrim_fake)))
+    print("loss generator", loss)
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+if __name__ == "__main__":
+    args = get_args()
+    gen = Generator().cuda()
+    disc = Discriminator().cuda()
+    prefix = "data_gan/"
+    os.makedirs(prefix, exist_ok=True)
+
+    train_model(
+        gen,
+        disc,
+        num_iterations=int(3e4),
+        batch_size=256,
+        prefix=prefix,
+        gen_loss_fn=compute_generator_loss,
+        disc_loss_fn=compute_discriminator_loss,
+        log_period=1000,
+        amp_enabled=not args.disable_amp,
+    )
---
+++ hw2/generative-modeling/gan/q1_5.py
@@ -0,0 +1,64 @@
+import os
+
+import torch
+from utils import get_args
+
+from networks import Discriminator, Generator
+from train import train_model
+
+
+def compute_discriminator_loss(
+    discrim_real, discrim_fake, discrim_interp, interp, lamb
+):
+    """
+    TODO 1.5.1: Implement WGAN-GP loss for discriminator.
+    loss = E[D(fake_data)] - E[D(real_data)] + lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
+    """
+    ##################################################################
+    # TODO 1.5: Implement WGAN-GP loss for discriminator.
+    # loss_pt1 = E[D(fake_data)] - E[D(real_data)]
+    # loss_pt2 = lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
+    # loss = loss_pt1 + loss_pt2
+    ##################################################################
+    # loss = None
+    # implement the loss from https://arxiv.org/pdf/1704.00028.pdf from Algorithm 1
+    loss1 = torch.mean(discrim_fake) - torch.mean(discrim_real)
+    loss2 = lamb * torch.mean((torch.norm(torch.autograd.grad(discrim_interp, interp, grad_outputs=torch.ones_like(discrim_interp), create_graph=True)[0], 2) - 1) ** 2)
+    loss = loss1 + loss2
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+def compute_generator_loss(discrim_fake):
+    ##################################################################
+    # TODO 1.5: Implement WGAN-GP loss for generator.
+    # loss = - E[D(fake_data)]
+    ##################################################################
+    loss = None
+    loss = -torch.mean(discrim_fake)
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+if __name__ == "__main__":
+    args = get_args()
+    gen = Generator().cuda()
+    disc = Discriminator().cuda()
+    prefix = "data_wgan_gp/"
+    os.makedirs(prefix, exist_ok=True)
+
+    train_model(
+        gen,
+        disc,
+        num_iterations=int(3e4),
+        batch_size=256,
+        prefix=prefix,
+        gen_loss_fn=compute_generator_loss,
+        disc_loss_fn=compute_discriminator_loss,
+        log_period=1000,
+        amp_enabled=not args.disable_amp,
+    )
